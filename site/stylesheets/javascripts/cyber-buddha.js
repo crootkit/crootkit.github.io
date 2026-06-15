f33781768586f25a74138b3a022662ae2577e444
hackerr1 <E5_2690V4_1@outlook.com>
2026-06-15 10:50:37 +0800

    更新了一下删除的内容，减少内存大小

--- site/stylesheets/javascripts/cyber-buddha.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  // 排除首页
-  if (document.documentElement.getAttribute("data-md-path") === "index") return;
-
-  const bg = document.createElement("div");
-  bg.id = "ad-bg";
-  bg.innerText = "广告位招租"; // 直接插入纯文本，最轻量
-  document.body.appendChild(bg);
-});
